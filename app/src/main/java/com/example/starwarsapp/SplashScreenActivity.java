84a422a10479e8ec5aeeb1a75463d313b2d64831
lulb8 <bonneville@et.esiea.fr>
2019-11-07 23:53:36 +0100

    Add Splashscreen

---
+++ app/src/main/java/com/example/starwarsapp/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.starwarsapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 1250;
+
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
