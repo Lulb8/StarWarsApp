51810e5120b99532ace8f1fab0c2593a1f8e1b42
lulb8 <bonneville@et.esiea.fr>
2019-11-24 14:59:09 +0100

    Adding camera and saving of pictures

--- app/src/main/java/com/example/starwarsapp/view/MainActivity.java
+++ app/src/main/java/com/example/starwarsapp/view/MainActivity.java
@@ -1,20 +1,44 @@
 package com.example.starwarsapp.view;
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import com.example.starwarsapp.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
+import java.io.FilePermission;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String NAME = "showTextView";
     private static final String PREFS = "PREFS";
+
+    // Define the pic id
+    private static final int pic_id = 123;
+    String pathToFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
 
         //When rotating the device
         if (savedInstanceState == null) {
@@ -64,6 +92,64 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_app_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_camera:
+                Intent camera_intent = new Intent();
+                camera_intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (camera_intent.resolveActivity(getPackageManager()) != null) {
+                    File photoFile = null;
+                    photoFile = createPhotoFile();
+
+                    if (photoFile != null) {
+                        pathToFile = photoFile.getAbsolutePath();
+                        Uri photoURI = FileProvider.getUriForFile(MainActivity.this, "com.example.starwarsapp.fileprovider", photoFile);
+                        camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(camera_intent, 1);
+                    }
+                }
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+            }
+        }
+    }
+
+    private File createPhotoFile() {
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(name, ".jpg", storageDir);
+        } catch (IOException e ) {
+            Log.d("myLog", "Excep : " + e.toString());
+        }
+        return image;
+    }
 
     @Override
     public void finish() {
